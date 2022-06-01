df0eee63827213db343443ca7ffee6ce9951cc8f
ucwealth <ucheokeke16@gmail.com>
2022-06-01 13:27:20 +0100

    replace scanner with buffered reader

--- config-file-parser/src/main/java/ConfigParser.java
+++ config-file-parser/src/main/java/ConfigParser.java
@@ -1,36 +1,78 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class ConfigParser {
-//    private FileReader file;
-//    FileReader file = new FileReader();
-//    BufferedReader buffer = new BufferedReader(file);
+    private static HashMap<String, String> map = new HashMap<>();
+    public ConfigParser(String filename, String[] args) {
+        String extension = "";
+        if(args.length < 1 || args[0].equalsIgnoreCase("production")) {
+            System.out.println("You are in production environment");
+        } else {
+            if(args[0].equalsIgnoreCase("staging")) {
+                System.out.println("You are in Staging Environment");
+                extension = ".staging";
+            } else if(args[0].equalsIgnoreCase("dev") || args[0].equalsIgnoreCase("development")) {
+                System.out.println("You are in development environment.");
+                extension = ".staging";
+            } else {
+                System.out.println("Wrong option provided!");
+            }
+        }
 
-//    public ConfigParser(FileReader file) {
-//        this.file = file;
-//    }
+        File file = new File("src/main/resources/" + filename + extension );
+        parseToMap(file);
+        getConfigFileDetails();
+    }
 
-    public ConfigParser() {
+    String parseToMap(File file) {
+        try {
+//            Scanner scan = new Scanner(file);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            String prefix = "";
+            int count = 0;
+
+            while((line = br.readLine()) != null) {
+                if(line.isEmpty()) {
+                    prefix = "";
+                } else if(line.startsWith("[")) {
+                    count++;
+                    prefix = line.substring(1, line.length() - 1) + count + ".";
+                } else {
+                   String[] splitLine = line.split("=");
+                   map.put(prefix + splitLine[0], splitLine[1]);
+                }
+            }
+
+
+        } catch(IOException e) {
+            System.out.println("File not found...Check well chief!");
+//            throw new RuntimeException(e);
+            e.printStackTrace();
+        }
+        System.out.println(map);
+        return "Config file parsed successfully!";
+    }
+
+    void getConfigFileDetails() {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("\nType 'All' too see all the contents of this file\nType a key to see it's value\nType 'q' to quit. ");
+            String input = scanner.nextLine();
+            if(input.equalsIgnoreCase("All")) {
+                for(String key : map.keySet()) {
+                    System.out.println(key +" = "+ map.get(key));
+                }
+            } else if(map.containsKey(input.toLowerCase())) {
+                System.out.println(map.get(input));
+            } else {
+                System.out.println("Ok, bye!");
+                break;
+            }
+        }
 
     }
 
-//    Scanner scan = new Scanner(System.in);
-//    String input = scan.nextLine();
-
 }
-//dbname=sq04_db
-//        host=127.0.0.1
-//
-//        [application]
-//        name=fintek-staging
-//        port=8081
-//        context-url=/api/v1
-//
-//        mode=staging
-//        theme=blue
-//        pipeline=fast-staging
-//
-//        [application]
-//        name=fintrack-staging
--- config-file-parser/src/main/java/Main.java
+++ config-file-parser/src/main/java/Main.java
@@ -5,35 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        String chosenFile = "";
-        if(Objects.equals(args[0], "staging")) {
-           chosenFile = "../resources/config.txt.staging";
-        } else if(Objects.equals(args[0], "dev")) {
-            chosenFile = "../resources/config.txt.dev";
-        } else if(Objects.equals(args[0], "")) {
-            chosenFile = "../resources/config.txt";
-        } else {
-            System.out.println("Please enter the correct option!");
-        }
-
-        try {
-            File file = new File(chosenFile);
-            Scanner scan = new Scanner(file);
-
-            HashMap<String, String> map = new HashMap<String, String>();
-            while(scan.hasNextLine()) {
-                String input = scan.nextLine();
-
-                String[] splitInput = input.split("=");
-                map.put(splitInput[0], splitInput[1]);
-                System.out.println(map);
-            }
-
-        } catch (IOException e) {
-            System.out.println("File not found...Check well chief!");
-            throw new RuntimeException(e);
-        }
-//        ConfigParser config = new ConfigParser("");
+        ConfigParser config = new ConfigParser("config.txt", args);
     }
 
 }
