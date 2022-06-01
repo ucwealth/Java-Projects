f568a5b339556f0c4821f81e59933988488cf987
ucwealth <ucheokeke16@gmail.com>
2022-06-01 10:40:51 +0100

    read from config file

---
+++ config-file-parser/src/main/java/ConfigParser.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class ConfigParser {
+//    private FileReader file;
+//    FileReader file = new FileReader();
+//    BufferedReader buffer = new BufferedReader(file);
+
+//    public ConfigParser(FileReader file) {
+//        this.file = file;
+//    }
+
+    public ConfigParser() {
+
+    }
+
+//    Scanner scan = new Scanner(System.in);
+//    String input = scan.nextLine();
+
+}
+//dbname=sq04_db
+//        host=127.0.0.1
+//
+//        [application]
+//        name=fintek-staging
+//        port=8081
+//        context-url=/api/v1
+//
+//        mode=staging
+//        theme=blue
+//        pipeline=fast-staging
+//
+//        [application]
+//        name=fintrack-staging
