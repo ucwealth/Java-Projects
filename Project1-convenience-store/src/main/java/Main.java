7d6595b1383daff9795db8817de9227cacad11d0
ucwealth <ucheokeke16@gmail.com>
2022-06-06 03:27:43 +0100

    read from excel file

--- Project1-convenience-store/src/main/java/Main.java
+++ Project1-convenience-store/src/main/java/Main.java
@@ -3,19 +3,20 @@ import com.cstore.model.Cashier;
 import com.cstore.model.Product;
 import com.cstore.storeEnums.Qualification;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("========== Welcome to SuperStore =========");
 
-        Product firstProduct = new Product(1, "Pomo", 1000.0);
-        System.out.println(firstProduct.toString());
+        Product.readProductFile();
 
         Applicant newCashier = new Applicant("100", "Lauren German", 28, Qualification.BSc);
         System.out.println(newCashier.toString());
 
 
-        Product myProduct = new Product(2, "Yam Flour", 3500.50 );
+        Product myProduct = new Product(2, "Eva Soap", "Soap", 10.05, 5);
         Cashier madu = new Cashier("Maduabuchi", myProduct);
     }
 
--- Project1-convenience-store/src/main/java/com/cstore/model/Product.java
+++ Project1-convenience-store/src/main/java/com/cstore/model/Product.java
@@ -1,28 +1,62 @@
 package com.cstore.model;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Product {
     private int id;
     private String productName;
+    private String productType;
     private double price;
-    public Product(int id, String productName, double price) {
+    private int quantity;
+    public Product(int id, String productName, String productType,  double price, int quantity) {
         this.id = id;
         this.productName = productName;
+        this.productType = productType;
         this.price = price;
+        this.quantity = quantity;
     }
 
     public String getProductName() {
         return productName;
     }
     public String setProductName(String productName) {
-       return this.productName = productName;
+        return this.productName = productName;
     }
+
+    public static void readProductFile() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("src/main/resources/products.csv"));
+        String line = "";
+        var productArray = new ArrayList<Product>();
+
+        try {
+            while((line = br.readLine()) != null) {
+                String[] lineArray = line.split(",");
+                var aProduct = new Product(Integer.parseInt(lineArray[0]), lineArray[1], lineArray[2], Double.parseDouble(lineArray[3]), Integer.parseInt(lineArray[4]));
+                productArray.add(aProduct);
+            }
+            System.out.println(productArray);
+
+        } catch(Exception e) {
+
+        }
+
+    }
+
+
+
 
     @Override
     public String toString() {
         return "Product{" +
                 "id='" + id + '\'' +
                 ", name='" + productName + '\'' +
-                ", price='" + price + '}';
+                ", type='" + productType + '\'' +
+                ", price='" + price + '\'' +
+                ", quantity='" + quantity + "'}";
     }
 
 }
